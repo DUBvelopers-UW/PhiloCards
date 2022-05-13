35f2017f5b524ff163c2d7fc063f455dc798f71f
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-05-12 20:52:57 -0700

    Index Page

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,33 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.main}>
-      <div>
-        <h1 className="PhiloHead">
-          PHILO
-          <br />
-          CARDS
-        </h1>
-        <p className="PhiloText">
-          so many things to <i>philo-bout</i>
-        </p>
+      <div className={styles.intro}>
 
-        <h3 className="WhatIsSection">WHAT IS PHILO CARDS?</h3>
-        <p>
-          If you're looking to get to know someone else better, learn new
-          perspectives and ideas, or to be introduced to philosophy, our is for
-          you! Philo Card is a student-found card game that transform the way
-          you can connect with somebody
-        </p>
+        <div className={styles.introTop}>
+          <h1 className={styles.philoHead}>
+            PHILO
+            <br/>
+            <span className={styles.shiftLeft}>CARDS</span>
+          </h1>
+          <p className={styles.philoText}>
+            so many things to <i>philo-bout</i>
+          </p>
+        </div>
+
+        <div className={styles.introBottom}>
+          <h3 className={`${styles.coreHeader} ${styles.center}`}>WHAT IS PHILO CARDS?</h3>
+          <p>
+            If you're looking to get to know someone else better, learn new
+            perspectives and ideas, or to be introduced to philosophy, our is for
+            you! Philo Card is a student-found card game that transform the way
+            you can connect with somebody
+          </p>
+        </div>
+
       </div>
 
-      <div className="grey">
-        <h3>HOW TO PLAY</h3>
+      <div className={styles.grey}>
+        <h3 className={`${styles.coreHeader} ${styles.shiftRight}`}>HOW TO PLAY</h3>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
           dolor quidem vero possimus! Omnis asperiores, fuga quisquam ducimus,
