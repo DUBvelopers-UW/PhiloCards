16446539d91b7b84ec6cee976adb84e4769b0297
jcha0713 <joocha0713@gmail.com>
2022-05-17 16:55:02 -0700

    Add logo icon to Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Navbar.module.scss";
 
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.logo}>
+        <Image src="/image/logo.svg" height={48} width={48} />
         <span className={styles.title}>philo</span>
       </div>
       <div className={styles.links}>
