86ce91a074ec1cd4c1cfa7f2d4aa58479e1f1637
jcha0713 <joocha0713@gmail.com>
2022-05-23 11:50:03 -0700

    Fix style in iframe element

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
-import styles from '../styles/About.module.css'
-import { AddressMap } from './map'
+import styles from "../styles/About.module.css";
+import { AddressMap } from "./map";
 export default function About() {
   return (
     <>
@@ -8,19 +8,21 @@ export default function About() {
         <ul className={styles.team}>
           <li className={styles.member}>
             <div className={styles.overlay} text-content="placeholder text">
-              <img src="image\cloverProfile.JPG" className={styles.image} />
+              <img src="image/cloverProfile.JPG" className={styles.image} />
             </div>
             <p className={styles.name}>
               <b>Clover Li</b>
-            </p><p>CEO</p>
+            </p>
+            <p>CEO</p>
           </li>
           <li className={styles.member}>
             <div className={styles.overlay} text-content="placeholder text">
               <img src="image/vishankProfile.jpeg" className={styles.image} />
             </div>
             <p className={styles.name}>
-              <b>Vishank Rughwani</b></p><p>CFO
+              <b>Vishank Rughwani</b>
             </p>
+            <p>CFO</p>
           </li>
         </ul>
         <ul className={styles.team}>
@@ -30,7 +32,8 @@ export default function About() {
             </div>
             <p className={styles.name}>
               <b>Kahn Wang</b>
-            </p><p>CTO</p>
+            </p>
+            <p>CTO</p>
           </li>
           <li className={styles.member}>
             <div className={styles.overlay} text-content="placeholder text">
@@ -38,7 +41,8 @@ export default function About() {
             </div>
             <p className={styles.name}>
               <b>Scott Ni</b>
-            </p><p>CMO</p>
+            </p>
+            <p>CMO</p>
           </li>
           <li className={styles.member}>
             <div className={styles.overlay} text-content="placeholder text">
@@ -46,24 +50,25 @@ export default function About() {
             </div>
             <p className={styles.name}>
               <b>Jason Weng</b>
-            </p><p>COO</p>
+            </p>
+            <p>COO</p>
           </li>
         </ul>
       </div>
 
-      <div className={styles.location_section}>
+      <div className={styles["location_section"]}>
         <h1 className={styles.title}>LOCATION</h1>
-        <AddressMap/>
-        
+        <AddressMap />
+
         <p>
           If you lived near the University of Washington, we can deliver the
           product to you! Contact us via Email or by text! Address: 4801 24th NE
           Seattle, WA 98105 Phone #+1 2062948126
         </p>
       </div>
-      <div className={styles.social_section}>
+      <div className={styles["social_section"]}>
         <h1 className={styles.title}>SOCIAL MEDIA</h1>
       </div>
     </>
-  )
+  );
 }
