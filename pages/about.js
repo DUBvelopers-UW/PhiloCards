5a68b9a6f83f6839fc6a0f6f12d3f266202ef4fa
Jenny Peng <jennypscale@gmail.com>
2022-05-25 01:25:06 -0700

    edit location section

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import { AddressMap } from "./map";
 export default function About() {
   return (
     <>
-      <div className={styles.team}>
+      <div className={styles["about_section"]}>
         <h1 className={styles.title}>ABOUT THE TEAM</h1>
         <ul className={styles.team}>
           <li className={styles.member}>
@@ -58,13 +58,16 @@ export default function About() {
 
       <div className={styles["location_section"]}>
         <h1 className={styles.title}>LOCATION</h1>
-        <AddressMap />
-
-        <p>
-          If you lived near the University of Washington, we can deliver the
-          product to you! Contact us via Email or by text! Address: 4801 24th NE
-          Seattle, WA 98105 Phone #+1 2062948126
+        <ul className={styles.location_info}>
+          <li><AddressMap/></li>
+          <li className={styles.location_text}>
+              <p className={styles.p_location}>
+              If you lived near the University of Washington, we can deliver the
+              product to you! Contact us via Email or by text! <br /><br />Address: 4801 24th NE
+              Seattle, WA 98105 <br /><br />Phone #+1 2062948126
         </p>
+          </li>
+        </ul>
       </div>
       <div className={styles["social_section"]}>
         <h1 className={styles.title}>SOCIAL MEDIA</h1>
